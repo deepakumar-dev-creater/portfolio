0756f073b68aceded7a6ddb3e7d3fb79a580e9d4
deepakumar-dev-creater <sean4deepak@gmail.com>
2021-11-14 22:04:05 +0530

    added animation

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -1,20 +1,117 @@
 import React, { useRef, useEffect, useState } from "react";
 import { gsap, Linear } from "gsap";
-export default function Skills() {
-  let [gsapCount, setGsapCount] = useState(0);
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-  const p1 = useRef();
+gsap.registerPlugin(ScrollTrigger);
+export default function Skills() {
+  let [pCount, setpCount] = useState(0);
+  let [cCount, setcCount] = useState(0);
+  let [mCount, setmCount] = useState(0);
+  let [jCount, setjCount] = useState(0);
+  let [rCount, setrCount] = useState(0);
+  let [aCount, setaCount] = useState(0);
+
+  const python = useRef();
+  const c = useRef();
+  const mysql = useRef();
+  const javascript = useRef();
+  const react = useRef();
+  const angular = useRef();
+
   useEffect(() => {
-    gsap.to(p1.current, {
+    gsap.to(python.current, {
+      scrollTrigger:{
+        start: "30% 50%",
+        markers:true,
+            
+          },
       width: "80%",
-      ease: Linear.easeNone,
+      ease: Linear.easeIn,
       duration: "3",
-     onUpdate:()=>{
-      console.log(gsapCount)
-      setGsapCount(gsapCount++)
-     }
+      onUpdate: () => {
+        if (pCount <= 80) {
+          setpCount(pCount++);
+        }
+      },
+    });
+    gsap.to(c.current, {
+      scrollTrigger:{
+        start: "30% 50%",
+        markers:true,
+            
+          },
+      width: "40%",
+      ease: Linear.easeIn,
+      duration: "3",
+      onUpdate: () => {
+        if (cCount <= 40) {
+          setcCount(cCount++);
+        }
+      },
+    });
+    gsap.to(mysql.current, {
+      scrollTrigger:{
+        start: "30% 50%",
+        markers:true,
+            
+          },
+      width: "70%",
+      ease: Linear.easeIn,
+      duration: "3",
+      onUpdate: () => {
+        if (mCount <= 70) {
+          setmCount(mCount++);
+        }
+      },
+    });
+    gsap.to(javascript.current, {
+      scrollTrigger:{
+    start: "30% 50%",
+    markers:true,
+        
+      },
+      width: "80%",
+      ease: Linear.easeIn,
+      duration: "3",
+      onUpdate: () => {
+        if (jCount <= 60) {
+          setjCount(jCount++);
+        }
+      },
+    });
+    gsap.to(react.current, {
+      scrollTrigger:{
+        start: "30% 50%",
+        end:"40%",
+        markers:true,
+            toggleActions:"play pause resume reset"
+          },
+      width: "50%",
+      ease: Linear.easeIn,
+      duration: "3",
+      onUpdate: () => {
+        if (rCount <= 50) {
+          setrCount(rCount++);
+        }
+      },
+    });
+    gsap.to(angular.current, {
+      scrollTrigger:{
+        start: "30% 50%",
+        markers:true,
+            
+          },
+      width: "30%",
+      ease: Linear.easeIn,
+      duration: "3",
+      onUpdate: () => {
+        if (aCount <= 30) {
+          setaCount(aCount++);
+        }
+      },
     });
   }, []);
+
   return (
     <div>
       <section>
@@ -23,65 +120,70 @@ export default function Skills() {
           <div className="grid grid-cols-1 mx-auto max-w-7xl md:grid-cols-2">
             <div className="flex flex-col p-10 space-y-3">
               <div className="flex justify-between">
-                <div>HTML</div>
-                <div> {gsapCount}%</div>
+                <div>Python</div>
+                <div> {pCount}%</div>
               </div>
               <div className="relative min-w-full bg-gray-200 rounded sm:w-1/2">
                 <div
-                  ref={p1}
-                  style={{width:"0%"}}
+                  ref={python}
+                  style={{ width: "0%" }}
                   className="absolute top-0 h-4 rounded shim-green"
                 ></div>
               </div>
               <div className="flex justify-between">
-                <div>HTML</div>
-                <div>98%</div>
+                <div>C++</div>
+                <div>{cCount}%</div>
               </div>
               <div className="relative min-w-full bg-gray-200 rounded sm:w-1/2">
                 <div
-                  style={{ width: "80%" }}
+                  ref={c}
+                  style={{ width: "0%" }}
                   className="absolute top-0 h-4 rounded shim-blue"
                 ></div>
               </div>
               <div className="flex justify-between">
-                <div>HTML</div>
-                <div id="count">98%</div>
+                <div>MYSQL</div>
+                <div>{mCount}%</div>
               </div>
               <div className="relative min-w-full bg-gray-200 rounded sm:w-1/2">
                 <div
-                  style={{ width: "100%" }}
+                  ref={mysql}
+                  style={{ width: "0%" }}
                   className="absolute top-0 h-4 rounded shim-red"
                 ></div>
               </div>
             </div>
             <div className="flex flex-col p-10 space-y-3">
               <div className="flex justify-between">
-                <div>HTML</div>
-                <div>98%</div>
+                <div>Javascript</div>
+                <div>{jCount}%</div>
               </div>
               <div className="relative min-w-full bg-gray-200 rounded sm:w-1/2">
                 <div
-                  style={{ width: "60%" }}
+                  ref={javascript}
+                  style={{ width: "0%" }}
                   className="absolute top-0 h-4 rounded shim-green"
                 ></div>
               </div>
               <div className="flex justify-between">
-                <div>HTML</div>
-                <div>98%</div>
+                <div>React</div>
+                <div>{rCount}%</div>
               </div>
               <div className="relative min-w-full bg-gray-200 rounded sm:w-1/2">
                 <div
-                  style={{ width: "80%" }}
+                  ref={react}
+                  style={{ width: "0%" }}
                   className="absolute top-0 h-4 rounded shim-blue"
                 ></div>
               </div>
               <div className="flex justify-between">
-                <div>HTML</div>
-                <div>98%</div>
+                <div>Angular</div>
+                <div>{aCount}%</div>
               </div>
               <div className="relative min-w-full bg-gray-200 rounded sm:w-1/2">
                 <div
-                  style={{ width: "100%" }}
+                  ref={angular}
+                  style={{ width: "0%" }}
                   className="absolute top-0 h-4 rounded shim-red"
                 ></div>
               </div>
