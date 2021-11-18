5b403d2fd0b09d49e31730b94c2a3b05583a3d0f
deepakumar-dev-creater <sean4deepak@gmail.com>
2021-11-18 20:24:32 +0530

    added resp

--- src/component/Services.js
+++ src/component/Services.js
@@ -5,60 +5,71 @@ import {
   faLinkedin,
   faSearchengin,
   faServicestack,
-  FaMobileAlt,  
-  faMedapps
+  FaMobileAlt,
+  faMedapps,
 } from "@fortawesome/free-brands-svg-icons";
 import React from "react";
+// faHtml5
 export default function Services() {
   return (
-    <div className="bg-fixed bg-cover bg-services">
+    <section className="bg-fixed bg-cover bg-services">
       <h2>Services</h2>
 
-      <div class="wrapper gap-y-10 text-lg">
-        <div class="single-service">
-          {/* <div class="social">
-            <i class="fa fa-codepen"></i>
-          </div> */}
-          <FontAwesomeIcon
-            icon={faHtml5}
-            className="mx-2 mt-2 text-blue-450 lg:text-4xl sm:text-2xl"
-          />
-          <span></span>
-          <h3>Web Development</h3>
+      <div class="wrapper gap-y-10  pop ">
+        <div class="single-service grid">
+          <div className="m-auto">
+            {" "}
+            <FontAwesomeIcon
+              icon={faHtml5}
+              className="mx-2 mt-2 text-blue-450 lg:text-6xl sm:text-2xl"
+            />
+          </div>
+
+          <h3 className="font-bold">Web Development</h3>
         </div>
-        <div class="single-service">
-        <FontAwesomeIcon
-            icon={faAngrycreative}
-            className="mx-2 mt-2 text-blue-450 lg:text-4xl sm:text-2xl"
-          />
-          <span></span>
-          <h3>ui / ux design</h3>
+        <div class="single-service grid">
+          <div className="m-auto">
+            {" "}
+            <FontAwesomeIcon
+              icon={faAngrycreative}
+              className="mx-2 mt-2  text-blue-450 lg:text-6xl text-4xl"
+            />
+          </div>
+
+          <h3 className="font-bold">ui / ux design</h3>
         </div>
-        <div class="single-service">
-        <FontAwesomeIcon
-            icon={faServicestack}
-            className="mx-2 mt-2 text-blue-450 lg:text-4xl sm:text-2xl"
-          />
-          <span></span>
-          <h3>API Development</h3>
+        <div class="single-service grid">
+          <div className="m-auto">
+            <FontAwesomeIcon
+              icon={faServicestack}
+              className="mx-2 mt-2 text-blue-450 lg:text-6xl sm:text-2xl"
+            />
+          </div>
+
+          <h3 className="font-bold">API Development</h3>
         </div>
-        <div class="single-service">
-        <FontAwesomeIcon
-            icon={faSearchengin}
-            className="mx-2 mt-2 lg:text-4xl sm:text-2xl text-blue-450"
-          />
-          <span></span>
-          <h3>SEO optimation</h3>
+        <div class="single-service grid">
+          <div className="m-auto">
+            <FontAwesomeIcon
+              icon={faSearchengin}
+              className="mx-2 mt-2 lg:text-6xl sm:text-2xl text-blue-450"
+            />
+          </div>
+
+          <h3 className="font-bold">SEO optimation</h3>
         </div>
-        <div class="single-service">
-        <FontAwesomeIcon
-            icon={faMedapps}
-            className="mx-2 mt-2 text-blue-450 lg:text-4xl sm:text-2xl"
-          />
-          <span></span>
-          <h3>App Development</h3>
+        <div class="single-service grid">
+          <div className="m-auto">
+            {" "}
+            <FontAwesomeIcon
+              icon={faMedapps}
+              className="mx-2 mt-2 text-blue-450 lg:text-6xl sm:text-2xl"
+            />
+          </div>
+
+          <h3 className="font-bold">App Development</h3>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
